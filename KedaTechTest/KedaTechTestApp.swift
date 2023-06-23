d8e9634a47f393d7a3e4b71f6598951b9eb8a46e
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-06-23 15:19:15 +0700

    Initial Commit

---
+++ KedaTechTest/KedaTechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KedaTechTestApp.swift
+//  KedaTechTest
+//
+//  Created by Marshall Kurniawan on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct KedaTechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
